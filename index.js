781be3a71f838898118aeddf9889c43c5e2c0f39
Bruno Pimentel <brunoapimentel@gmail.com>
2018-02-07 13:48:48 -0200

    Páginas home, contato, blog e quem somos

---
+++ index.js
@@ -0,0 +1,30 @@
+//Inclusão de bibliotecas
+const express = require('express');
+const app = express();
+
+//Configurações
+app.set('view engine', 'ejs');
+app.use('/site', express.static('public'));
+
+//Rotas
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/quem-somos', (req, res) => {
+    res.render('quem-somos');
+});
+
+app.get('/blog', (req, res) => {
+    res.render('blog');
+});
+
+app.get('/contato', (req, res) => {
+    res.render('contato');
+});
+
+
+//Listen
+app.listen(3000, () => {
+    console.log('Servidor escutando na porta 3000');
+});
